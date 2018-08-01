4e134bef5b89c76673b97e6b913eeb8358169eee
Jomon Johnson <jomon.entero@gmail.com>
2018-08-01 20:36:36 +0530

    - Scroll Reveal Asset Bundle added - bower dependency added
    Signed-off-by: Jomon Johnson <jomon.entero@gmail.com>

---
+++ src/ScrollrevealToAsset.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 8/1/18
+ * Time: 1:59 PM
+ */
+
+namespace entero\scrollreveal;
+
+class ScrollrevealToAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/scrollreveal/dist/';
+    public $js = [
+        'scrollreveal.min.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
